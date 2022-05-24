d85c9907d37806565f1e68dd6be9c0346e008a12
GuilhermeHPSilva <67592214+GHPSseaking@users.noreply.github.com>
2022-05-24 12:54:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Desenvolver um algoritmo que leia a cotação do euro converta para o Real, calcule a compra de 750 euros e imprima o resultado; 
+
+double euroPReal = 5.20;
+
+double resultadoCompra = 750 * euroPReal;
+
+Console.WriteLine($"A compra de 750 euros ficaria: {resultadoCompra} em reais"); 
